4da88af84f2d79e62f209a4bb5c09889cf41c5a1
Zebo Li <zebo@ginkgo-llc.com>
2017-12-07 09:48:25 -0800

    sdf

--- pkugoodspeed/src/cnn.py
+++ pkugoodspeed/src/cnn.py
@@ -40,6 +40,18 @@ from keras.layers import Flatten, Conv2D, MaxPooling2D
 from keras.optimizers import SGD, Adam, RMSprop, Adadelta
 from keras.utils import np_utils, plot_model
 
+hyper_pwr = 0.0
+hyper_train_ratio = 0.6
+hypter_n = 25
+hypter_m = 16
+hypter_NR = 256
+hypter_NC = 128
+hypter_delta = 1.
+hyper_dropout1 = 0.1
+hyper_dropout2 = 0.1
+hyper_dropout3 = 0.25
+hyper_dropout4 = 0.25
+
 
 ## Function for loading the audio data, return a dataFrame
 def load_audio_data(path):
@@ -146,18 +158,20 @@ if __name__ == '__main__':
     
     ## Parsing the data Frame into train and test sets
     print("SPLITTING DATA INTO TRAIN AND TEST SETS!")
-    tr_x, tr_y, ts_x, ts_y, idmap = train_test_split(raw_df, ratio=0.9)
+    tr_x, tr_y, ts_x, ts_y, idmap = train_test_split(raw_df, ratio=hyper_train_ratio)
     
     ## Preprocessing x data
     print("PROCESSING FFT!")
-    train_x = fft_convert(tr_x, rate = 16000, n = 25, m = 16, NR = 256, NC = 128, delta = 1.)
-    test_x = fft_convert(ts_x, rate = 16000, n = 25, m = 16, NR = 256, NC = 128, delta = 1.)
+    train_x = fft_convert(tr_x, rate = 16000, n = hypter_n, m = hypter_m, 
+    NR = hypter_NR, NC = hypter_NC, delta = hypter_delta)
+    test_x = fft_convert(ts_x, rate = 16000, n = hypter_n, m = hypter_m, 
+    NR = hypter_NR, NC = hypter_NC, delta = hypter_delta)
     img_r, img_c = np.shape(train_x)[1:]
     train_x = train_x.reshape(len(train_x), img_r, img_c, 1)
     test_x = test_x.reshape(len(test_x), img_r, img_c, 1)
     
     ## Compute class weights
-    cls_wts = comp_cls_wts(tr_y, pwr = 0.0)
+    cls_wts = comp_cls_wts(tr_y, pwr = hyper_pwr)
     
     ## Preprocessing y data
     n_cls = 31
@@ -176,19 +190,19 @@ if __name__ == '__main__':
     model.add(Conv2D(16, kernel_size = (4, 4), padding = 'same'))
     model.add(MaxPooling2D(pool_size = (2, 2)))
     model.add(Activation('relu'))
-    model.add(Dropout(0.1))
+    model.add(Dropout(hyper_dropout1))
     model.add(Conv2D(16, kernel_size = (4, 4), padding = 'same'))
     model.add(MaxPooling2D(pool_size = (2, 2)))
     model.add(Activation('relu'))
-    model.add(Dropout(0.1))
+    model.add(Dropout(hyper_dropout2))
     model.add(Conv2D(16, kernel_size = (4, 4), padding = 'same'))
     model.add(MaxPooling2D(pool_size = (2, 2)))
     model.add(Activation('relu'))
-    model.add(Dropout(0.25))
+    model.add(Dropout(hyper_dropout3))
     model.add(Flatten())
     model.add(Dense(128))
     model.add(Activation('relu'))
-    model.add(Dropout(0.25))
+    model.add(Dropout(hyper_dropout4))
     model.add(Dense(n_cls, activation = 'softmax'))
     model.summary()
     
