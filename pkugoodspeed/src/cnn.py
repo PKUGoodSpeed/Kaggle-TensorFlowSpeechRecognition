6e05e09eeb713811d15452cf527cc0c9d5e62b6c
Zebo Li <zebo@ginkgo-llc.com>
2017-12-14 12:16:43 -0800

    sdf

--- pkugoodspeed/src/cnn.py
+++ pkugoodspeed/src/cnn.py
@@ -48,7 +48,7 @@ hyper_NR = 208
 hyper_NC = 112
 hyper_delta = 0.5
 hyper_dropout1 = 0.2
-hyper_dropout2 = 0.4
+hyper_dropout2 = 0.5
 hyper_dropout3 = 0.64
 hyper_dropout4 = 0.56
 hyper_dropout5 = 0.5
@@ -248,7 +248,7 @@ if __name__ == '__main__':
     
     ### Train the model
     print("TRAINING BEGINS!")
-    N_epoch = 1
+    N_epoch = 40
     res = model.fit(train_x, train_y, batch_size = 128, epochs = N_epoch, 
     verbose = 1, validation_data = (test_x, test_y), 
     class_weight = cls_wts)
