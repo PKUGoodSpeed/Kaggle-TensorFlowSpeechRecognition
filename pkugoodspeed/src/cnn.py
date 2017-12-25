8bd23ccdc97c3b3db5b92cf4ba8bc722a0e9eb91
Zebo Li <zebo@ginkgo-llc.com>
2017-12-24 19:10:56 -0800

    sdf

--- pkugoodspeed/src/cnn.py
+++ pkugoodspeed/src/cnn.py
@@ -268,7 +268,7 @@ if __name__ == '__main__':
     
     ''' First training section '''
     ### Compile the model
-    N_epoch = 600
+    N_epoch = 700
     learning_rate = 0.0017
     decay_rate = 0.01
     momentum = 0.9
