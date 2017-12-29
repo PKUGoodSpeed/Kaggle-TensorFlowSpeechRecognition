0c91c5c8e5347d25a88ba52436e4af190ebb97eb
Zebo Li <zebo@ginkgo-llc.com>
2017-12-28 17:57:05 -0800

    sdf

--- pkugoodspeed/src/cnn.py
+++ pkugoodspeed/src/cnn.py
@@ -226,21 +226,21 @@ if __name__ == '__main__':
     model = Sequential()
     model.add(MaxPooling2D(pool_size = (2, 2), input_shape = (img_r, img_c, 1)))
     
-    model.add(Conv2D(100, kernel_size = (9, 9), padding = 'same'))
+    model.add(Conv2D(100, kernel_size = (6, 6), padding = 'same'))
     model.add(MaxPooling2D(pool_size = (2, 2)))
     model.add(LeakyReLU(alpha=0.01))
     #model.add(BatchNormalization())
     #model.add(Activation('relu'))
     model.add(Dropout(hyper_dropout1))
     
-    model.add(Conv2D(200, kernel_size = (7, 7), padding = 'same'))
+    model.add(Conv2D(200, kernel_size = (5, 5), padding = 'same'))
     model.add(MaxPooling2D(pool_size = (2, 2)))
     model.add(LeakyReLU(alpha=0.01))
     #model.add(BatchNormalization())
     #model.add(Activation('relu'))
     model.add(Dropout(hyper_dropout2))
     
-    model.add(Conv2D(400, kernel_size = (5, 5), padding = 'same'))
+    model.add(Conv2D(400, kernel_size = (4, 4), padding = 'same'))
     model.add(MaxPooling2D(pool_size = (2, 2)))
     model.add(LeakyReLU(alpha=0.01))
     #model.add(BatchNormalization())
@@ -257,12 +257,12 @@ if __name__ == '__main__':
     
     model.add(Flatten())
     
-    model.add(Dense(320))
+    model.add(Dense(256))
     #model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(Dropout(hyper_dropout5))
     
-    model.add(Dense(80))
+    model.add(Dense(64))
     #model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(Dropout(hyper_dropout5))
