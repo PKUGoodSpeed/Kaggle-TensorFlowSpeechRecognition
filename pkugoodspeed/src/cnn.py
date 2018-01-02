9d6c85ec9043902ebe8e5de6b49cc84751f93b1f
Zebo Li <zebo@ginkgo-llc.com>
2018-01-02 08:38:42 -0800

    sdf

--- pkugoodspeed/src/cnn.py
+++ pkugoodspeed/src/cnn.py
@@ -227,6 +227,7 @@ if __name__ == '__main__':
     model.add(Conv2D(32, kernel_size = (11, 11), input_shape = (img_r, img_c, 1), padding = 'same'))
     model.add(MaxPooling2D(pool_size = (2, 2)))
     model.add(LeakyReLU(alpha=0.01))
+    model.add(Dropout(hyper_dropout0))
     
     model.add(Conv2D(64, kernel_size = (9, 9), padding = 'same'))
     model.add(MaxPooling2D(pool_size = (2, 2)))
