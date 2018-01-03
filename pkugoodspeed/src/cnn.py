2e5bec0be0f9c70834e49ae2499490a5a24f86be
Zebo Li <zebo@ginkgo-llc.com>
2018-01-03 11:56:27 -0800

    sdf

--- pkugoodspeed/src/cnn.py
+++ pkugoodspeed/src/cnn.py
@@ -46,13 +46,13 @@ from keras.callbacks import LearningRateScheduler
 
 hyper_pwr = 0.5
 hyper_train_ratio = 0.8
-hyper_n = 25
-hyper_m = 15
-hyper_NR = 208
-hyper_NC = 112
+hyper_n = 20
+hyper_m = 6
+hyper_NR = 160
+hyper_NC = 80
 hyper_delta = 0.3
 hyper_dropout0 = 0.2
-hyper_dropout1 = 0.36
+hyper_dropout1 = 0.3
 hyper_dropout2 = 0.6
 hyper_dropout3 = 0.6
 hyper_dropout4 = 0.4
