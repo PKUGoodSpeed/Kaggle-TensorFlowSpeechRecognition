7feb974bf470961b591fe1247e7727cda2c139c9
Zebo Li <zebo@ginkgo-llc.com>
2018-01-11 16:44:11 -0800

    sdf

--- pkugoodspeed/src/all4fold.py
+++ pkugoodspeed/src/all4fold.py
@@ -33,36 +33,33 @@ mpl.rcParams['ytick.minor.size'] = 2
 from sklearn.utils import shuffle
 
 # Keras
-from keras import backend as K
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
 from keras.layers import Flatten, Conv2D, MaxPooling2D
 from keras.optimizers import SGD, Adam, RMSprop, Adadelta
 from keras.utils import np_utils, plot_model
-from keras.layers.normalization import BatchNormalization
-from keras.layers.advanced_activations import LeakyReLU, PReLU
-from keras.callbacks import LearningRateScheduler
 
-hyper_pwr = 0.3
+hyper_pwr = 0.4
 hyper_train_ratio = 0.88
-hyper_n = 16
-hyper_m = 0
-hyper_NR = 128
-hyper_NC = 64
+hyper_n = 20
+hyper_m = 10
+hyper_NR = 160
+hyper_NC = 96
 hyper_delta = 0.3
-hyper_dropout0 = 0.2
-hyper_dropout1 = 0.36
-hyper_dropout2 = 0.64
-hyper_dropout3 = 0.64
+hyper_dropout0 = 0.12
+hyper_dropout1 = 0.25
+hyper_dropout2 = 0.5
+hyper_dropout3 = 0.75
 hyper_dropout4 = 0.5
-hyper_dropout5 = 0.7
-N_NOISE = 600
+hyper_dropout5 = 0.6
+N_NOISE = 900
 
 TAGET_LABELS = ['yes', 'no', 'up', 'down', 'left', 'right', 'on', 'off', 'stop', 'go', 'silence', 'unknown']
 
+
 ## Function for loading the audio data, return a dataFrame
-def load_audio_data(path, ltoi):
+def load_audio_data(path):
     '''
     path: audio file path
     return: pd.DataFrame
@@ -70,24 +67,21 @@ def load_audio_data(path, ltoi):
     raw = {'x': [], 'y': [], 'label':[]}
     for i, folder in enumerate(os.listdir(path)):
         for filename in os.listdir(path + '/' + folder):
-            rate, sample = wavfile.read(path + '/' + folder + '/' + filename)
+            rate, sample = wavfile.read(data_dir + '/' + folder + '/' + filename)
             assert(rate == 16000)
             if folder == 'silence':
                 length = len(sample)
                 for j in range(int(length/rate)):
                     raw['x'].append(np.array(sample[j*rate: (j+1)*rate]))
-                    raw['y'].append(ltoi['silence'])
+                    raw['y'].append(i)
                     raw['label'].append('silence')
             else:
                 p = max(0, rate - len(sample))
                 sample = np.pad(sample, [(0,p)], mode='constant')
                 sample = sample[:rate]
                 raw['x'].append(np.array(sample))
-                label = folder
-                if folder not in TAGET_LABELS:
-                    label = 'unknown'
-                raw['y'].append(ltoi[label])
-                raw['label'].append(label)
+                raw['y'].append(i)
+                raw['label'].append(folder)
     return pd.DataFrame(raw)
     
 # Generating noise
@@ -139,15 +133,41 @@ def train_test_split(df, ratio = 0.7):
     test_y = []
     train_x = []
     train_y = []
+    label_map = {}
     for i in set(df.y.tolist()):
         tmp_df = df[df.y == i]
+        label_map[i] = tmp_df.label.tolist()[0]
         tmp_df = shuffle(tmp_df)
         tmp_n = int(len(tmp_df)*ratio)
         train_x += tmp_df.x.tolist()[: tmp_n]
         test_x += tmp_df.x.tolist()[tmp_n: ]
         train_y += tmp_df.y.tolist()[: tmp_n]
         test_y += tmp_df.y.tolist()[tmp_n: ]
-    return np.array(train_x), np.array(train_y), np.array(test_x), np.array(test_y)
+    return np.array(train_x), np.array(train_y), np.array(test_x), np.array(test_y), label_map
+
+# Split train, test sets, and also return label_map
+def four_fold_split(df):
+    x1 = []
+    x2 = []
+    x3 = []
+    x4 = []
+    y1 = []
+    y2 = []
+    y3 = []
+    y4 = []
+    for i in set(df.y.tolist()):
+        tmp_df = df[df.y == i]
+        tmp_df = shuffle(tmp_df)
+        tmp_n = int(len(tmp_df)*0.3)
+        x1 += tmp_df.x.tolist()[: tmp_n]
+        x2 += tmp_df.x.tolist()[tmp_n: 2*tmp_n]
+        x3 += tmp_df.x.tolist()[2*tmp_n: 3*tmp_n]
+        x4 += tmp_df.x.tolist()[3*tmp_n: ]
+        y1 += tmp_df.y.tolist()[: tmp_n]
+        y2 += tmp_df.y.tolist()[tmp_n: 2*tmp_n]
+        y3 += tmp_df.y.tolist()[2*tmp_n: 3*tmp_n]
+        y4 += tmp_df.y.tolist()[3*tmp_n: ]
+    return np.array(x1), np.array(x2), np.array(x3), np.array(x4), np.array(y1), np.array(y2), np.array(y3), np.array(y4)
 
 # Using fft to convert input x's
 def fft_convert(samples, rate = 16000, n = 25, m = 16, NR = 256, NC = 128, delta = 1.E-10):
@@ -165,7 +185,7 @@ def fft_convert(samples, rate = 16000, n = 25, m = 16, NR = 256, NC = 128, delta
         spec = np.pad(spec, [(0,p1), (0, p2)], mode='constant')
         spec = spec[:NR, :NC]
         res.append(spec)
-    return np.log(np.array(res) + delta)
+    return list(np.log(np.array(res) + delta))
     
 # Function to compute class weights
 def comp_cls_wts(y, pwr = 0.2):
@@ -178,7 +198,7 @@ def comp_cls_wts(y, pwr = 0.2):
     return dic
     
 # Get Prediction
-def getPrediction(model, path, mp):
+def getPrediction(model, path):
     files = os.listdir(path)
     files.sort()
     dic = {'fname':[], 'label':[], 'prob':[] }
@@ -203,9 +223,8 @@ def getPrediction(model, path, mp):
         ty = model.predict_classes(x, batch_size=128)
         sy = model.predict(x, batch_size=128)
         for j,p in enumerate(ty):
-            y.append(mp[p])
+            y.append(idmap[p])
             pro.append(1.*sy[j][p]/np.sum(sy[j]))
-        print "\n\n"
     dic['fname'] = files
     dic['label'] = y
     dic['prob'] = pro
@@ -214,7 +233,6 @@ def getPrediction(model, path, mp):
 
 
 if __name__ == '__main__':
-    print "WORK STATED!!:"
     data_dir = '../data/train/audio'
     ## change the name of `_background_noise_' into 'silence` which is a proper label name
     if os.path.exists(data_dir + '/' + '_background_noise_'):
@@ -239,6 +257,7 @@ if __name__ == '__main__':
     print "LOADING NOISE DATA FINISHED!"
     print "LOADING NOISY DATA..."
     raw_df = raw_df.append(load_audio_data('../data/new_data/augmented_dataset_verynoisy', label2idx), ignore_index=True)
+    print "LOADING NOISY DATA FINISHED!"
     print label2idx
     print idmap
     for lab in TAGET_LABELS:
@@ -246,18 +265,31 @@ if __name__ == '__main__':
     
     ## Parsing the data Frame into train and test sets
     print("SPLITTING DATA INTO TRAIN AND TEST SETS!")
-    train_x, train_y, test_x, test_y = train_test_split(raw_df, ratio=hyper_train_ratio)
+    x1, x2, x3, x4, y1, y2, y3, y4 = four_fold_split(raw_df)
     del raw_df
     
     ## Preprocessing x data
     print("PROCESSING FFT!")
-    train_x = fft_convert(train_x, rate = 16000, n = hyper_n, m = hyper_m, 
+    train_x = []
+    train_x += fft_convert(x1, rate = 16000, n = hyper_n, m = hyper_m, 
     NR = hyper_NR, NC = hyper_NC, delta = hyper_delta)
-    test_x = fft_convert(test_x, rate = 16000, n = hyper_n, m = hyper_m, 
+    del x1
+    train_x += fft_convert(x2, rate = 16000, n = hyper_n, m = hyper_m, 
     NR = hyper_NR, NC = hyper_NC, delta = hyper_delta)
+    del x2
+    train_x += fft_convert(x3, rate = 16000, n = hyper_n, m = hyper_m, 
+    NR = hyper_NR, NC = hyper_NC, delta = hyper_delta)
+    del x3
+    train_x = np.array(train_x)
+    test_x = fft_convert(x4, rate = 16000, n = hyper_n, m = hyper_m, 
+    NR = hyper_NR, NC = hyper_NC, delta = hyper_delta)
+    del x4
+    test_x = np.array(test_x)
     img_r, img_c = np.shape(train_x)[1:]
     train_x = train_x.reshape(len(train_x), img_r, img_c, 1)
     test_x = test_x.reshape(len(test_x), img_r, img_c, 1)
+    train_y = np.concatenate([y1, y2, y3])
+    test_y = y4
     
     ## Compute class weights
     cls_wts = comp_cls_wts(train_y, pwr = hyper_pwr)
@@ -277,6 +309,7 @@ if __name__ == '__main__':
     print("CONSTRUCTING MODEL!")
     model = Sequential()
     model.add(MaxPooling2D(pool_size = (2, 2), input_shape = (img_r, img_c, 1)))
+    #model.add(AveragePooling2D(pool_size = (2, 2), input_shape = (img_r, img_c, 1)))
     
     model.add(Conv2D(64, kernel_size = (9, 9), padding = 'same'))
     model.add(MaxPooling2D(pool_size = (2, 2)))
@@ -314,14 +347,20 @@ if __name__ == '__main__':
     model.add(Activation('relu'))
     model.add(Dropout(hyper_dropout5))
     
+    model.add(Dense(128))
+    #model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(hyper_dropout5))
+    
     model.add(Dense(n_cls, activation = 'softmax'))
     model.summary()
     
+    
     ''' First training section '''
     ### Compile the model
-    N_epoch = 240
-    learning_rate = 0.025
-    decay_rate = 1./1.25
+    N_epoch = 2
+    learning_rate = 0.03
+    decay_rate = 1./1.20
     optimizer = SGD(learning_rate)
     loss = 'categorical_crossentropy'
     metrics = ['accuracy']
@@ -334,7 +373,7 @@ if __name__ == '__main__':
     def scheduler(epoch):
         global learning_rate
         global decay_rate
-        if epoch%20 == 0:
+        if epoch%40 == 0:
             learning_rate *= decay_rate
             print("CURRENT LEARNING RATE = ", learning_rate)
         return learning_rate
@@ -354,49 +393,23 @@ if __name__ == '__main__':
     del test_x
     del test_y
     
-    ## Plot results
-    steps = [i for i in range(len(test_accu))]
-    
-    statics = test_accu[200:]
-    filename = "../cnn2_output/test_accu.txt"
+    statics = test_accu[0:]
+    filename = "../cnn_output/test_accu.txt"
     f = open(filename,'w')
     for acc in statics:
         f.write(str(acc) + ' ')
     f.write("\n" + str(sum(statics)*1./len(statics)))
     f.close()
     
-    statics = train_accu[200:]
-    filename = "../cnn2_output/train_accu.txt"
+    statics = train_accu[0:]
+    filename = "../cnn_output/train_accu.txt"
     f = open(filename,'w')
     for acc in statics:
         f.write(str(acc) + ' ')
     f.write("\n" + str(sum(statics)*1./len(statics)))
     f.close()
-    
-    
-    print("VISUALIZATION:")
-        ## Plotting the results
-    fig, axes = plt.subplots(2,2, figsize = (12, 12))
-    fig.subplots_adjust(hspace = 0.4, wspace = 0.4)
-
-    axes[0][0].set_title('Loss')
-    axes[0][0].plot(steps, train_loss, label = 'train loss')
-    axes[0][0].plot(steps, test_loss, label = 'test loss')
-    axes[0][0].set_xlabel('# of steps')
-    axes[0][0].legend()
-
-    axes[0][1].set_title('Accuracy')
-    axes[0][1].plot(steps, train_accu, label = 'train accuracy')
-    axes[0][1].plot(steps, test_accu, label = 'test accuracy')
-    axes[0][1].set_xlabel('# of steps')
-    axes[0][1].legend()
-    
-    plt.savefig('../cnn2_output/convrg_rst.png')
-    
-    ## show model configuration
-    plot_model(model, to_file = '../cnn2_output/model.png')
     
     ## Getting prediction
-    df = getPrediction(model, '../data/test/audio',idmap)
+    df = getPrediction(model, '../data/test/audio')
     df = df.set_index('fname')
-    df.to_csv('../cnn2_output/predict.csv')
+    df.to_csv('../cnn_output/predict.csv')
--- pkugoodspeed/src/main.py
+++ pkugoodspeed/src/main.py
@@ -243,7 +243,7 @@ if __name__ == '__main__':
     
     ### Train the model
     print("TRAINING BEGINS!")
-    N_epoch = 50
+    N_epoch = 600
     res = model.fit(train_x, train_y, batch_size = 128, epochs = N_epoch, 
     verbose = 1, validation_data = (test_x, test_y), 
     class_weight = cls_wts)
